dac927d0a00b50244c925884e03fd577c8dd5542
Lawrence Chu <DOLLARS1912@10-140-62-61.wifi-m.deakin.edu.au>
2017-04-27 13:30:45 +1000

    week7test

--- ViewController.swift
+++ ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // HELLO
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
